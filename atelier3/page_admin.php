f5b1bc7889394e32ae6748372130d8d6f9afa471
bstocker <boris.stocker@gmail.com>
2024-12-11 13:16:17 +0100

    Update page_admin.php

--- atelier3/page_admin.php
+++ atelier3/page_admin.php
@@ -17,7 +17,7 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     <title>Page protégée</title>
 </head>
 <body>
-    <h1>Bienvenue sur la page administrateur</h1>
+    <h1>Bienvenue sur la page administrateur de l'atelier 3</h1>
     <p>Vous êtes connecté en tant que : <?php echo htmlspecialchars($_SESSION['username']); ?></p>
     <a href="logout.php">Se déconnecter</a>
 </body>
