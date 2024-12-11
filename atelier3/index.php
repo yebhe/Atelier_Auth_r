8375b06b3ecffc5f8603797098e6ac1038ed1435
bstocker <boris.stocker@gmail.com>
2024-12-11 13:08:29 +0100

    Update index.php

--- atelier3/index.php
+++ atelier3/index.php
@@ -35,7 +35,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <title>Connexion</title>
 </head>
 <body>
-    <h2>Page de connexion </h2>
+    <h1>Atelier authentification par Session</h1>
+    <h3>La page <a href="page_admin.php">page_admin.php</a> de cet atelier 3 est inaccéssible tant que vous ne vous serez pas connecté avec le login 'admin' et mot de passe 'secret'</h3>
     <form method="POST" action="">
         <label for="username">Nom d'utilisateur :</label>
         <input type="text" id="username" name="username" required>
