447ed4d6b35ea4a931d315ecf85b0ea446059756
bstocker <boris.stocker@gmail.com>
2024-12-11 13:11:22 +0100

    Create page_admin.php

---
+++ atelier3/page_admin.php
@@ -0,0 +1,24 @@
+<?php
+// Démarrer la session
+session_start();
+
+// Vérifier si l'utilisateur s'est bienconnecté
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+    header('Location: index.php'); // Dans le cas contraire, l'utilisateur sera redirigé vers la page de connexion
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page protégée</title>
+</head>
+<body>
+    <h1>Bienvenue sur la page administrateur</h1>
+    <p>Vous êtes connecté en tant que : <?php echo htmlspecialchars($_SESSION['username']); ?></p>
+    <a href="logout.php">Se déconnecter</a>
+</body>
+</html>
