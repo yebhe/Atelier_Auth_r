a59174bddd5e222c79051f052c8f9fb4f9e33205
bstocker <boris.stocker@gmail.com>
2024-12-11 09:31:10 +0100

    Create home.php

---
+++ atelier2/home.php
@@ -0,0 +1,25 @@
+<?php
+// Démarrer la session
+session_start();
+
+// Vérifier si l'utilisateur est bien en possession d'un cookie valide
+// Dans le cas contraire il sera redirigé vers la page d'accueil de connexion
+if (!isset($_COOKIE['authToken']) || $_COOKIE['authToken'] !== '12345') {
+    header('Location: index.php');
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+</head>
+<body>
+    <h1>Bienvenue sur la page protégée par un Cookie</h1>
+    <p>Vous êtes connecté en tant qu'admin.</p>
+    <a href="logout.php">Se déconnecter</a>
+</body>
+</html>
