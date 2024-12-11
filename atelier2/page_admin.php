cff50ab8e088deefdb0c0a2c91f240ba03cc003d
bstocker <boris.stocker@gmail.com>
2024-12-11 11:17:43 +0100

    Update page_admin.php

--- atelier2/page_admin.php
+++ atelier2/page_admin.php
@@ -4,7 +4,7 @@ session_start();
 
 // Vérifier si l'utilisateur est bien en possession d'un cookie valide
 // Dans le cas contraire il sera redirigé vers la page d'accueil de connexion
-if (!isset($_COOKIE['authToken']) || $_COOKIE['authToken'] !== 'secret') {
+if (!isset($_COOKIE['authToken']) || $_COOKIE['authToken'] !== '12345') {
     header('Location: index.php');
     exit();
 }
@@ -18,7 +18,7 @@ if (!isset($_COOKIE['authToken']) || $_COOKIE['authToken'] !== 'secret') {
     <title>Accueil</title>
 </head>
 <body>
-    <h1>Bienvenue sur la page protégée par un Cookie</h1>
+    <h1>Bienvenu sur la page Administrateur protégée par un Cookie</h1>
     <p>Vous êtes connecté en tant qu'admin.</p>
     <a href="logout.php">Se déconnecter</a>
 </body>
