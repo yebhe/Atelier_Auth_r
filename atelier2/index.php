c597d8ab41adcfc58e0599f7cd981cf1e5cb1e1b
bstocker <boris.stocker@gmail.com>
2024-12-11 11:16:41 +0100

    Update index.php

--- atelier2/index.php
+++ atelier2/index.php
@@ -1,11 +1,11 @@
 <?php
-// Démarrer une session utilisateur
+// Démarrer une session utilisateur qui sera en mesure de pouvoir gérer les Cookies
 session_start();
 
 // Vérifier si l'utilisateur est déjà en possession d'un cookie valide (cookie authToken ayant le contenu 12345)
 // Si l'utilisateur possède déjà ce cookie, il sera redirigé automatiquement vers la page home.php
 // Dans le cas contraire il devra s'identifier.
-if (isset($_COOKIE['authToken']) && $_COOKIE['authToken'] === 'secret') {
+if (isset($_COOKIE['authToken']) && $_COOKIE['authToken'] === '12345') {
     header('Location: page_admin.php');
     exit();
 }
