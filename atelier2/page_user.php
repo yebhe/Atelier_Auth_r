1b29b59b36ce7dc899aec3ad8261261d7d86d257
yebhe <1lamarana.mamadou@gmail.com>
2025-11-05 10:33:09 +0100

    Add user page with cookie session validation

--- atelier2/page_user.php
+++ atelier2/page_user.php
@@ -1,1 +1,25 @@
+<?php
+// Démarrer la session
+session_start();
 
+// Vérifier si l'utilisateur est bien en possession d'un cookie valide
+// Dans le cas contraire il sera redirigé vers la page d'accueil de connexion
+if (!isset($_COOKIE['authToken']) || $_COOKIE['authToken'] !== $_SESSION['token'] ) {
+    header('Location: index.php');
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+</head>
+<body>
+    <h1>Bienvenue sur la page user protégée par un Cookie <?=$_SESSION['token'] ?></h1>
+    <p>Vous êtes connecté en tant que user</p>
+    <a href="logout.php">Se déconnecter</a>
+</body>
+</html>
